c1bac0d20ea8f57e7d866265328e028ccdd7090e
Gabriel LE GALL <71171030+gabriellegall@users.noreply.github.com>
2025-02-28 16:42:52 +0100

    Update agg_games_with_phases.sql

--- models/datamart/agg_games_with_phases.sql
+++ models/datamart/agg_games_with_phases.sql
@@ -4,12 +4,12 @@ SELECT
     username,
     game_uuid,
     game_phase,
-    end_time,
-    end_time_month, 
-    opponent_rating_range,
-    playing_as
-    playing_result,
-    time_class,
+    ANY_VALUE(end_time) AS end_time,
+    ANY_VALUE(end_time_month) AS end_time_month, 
+    ANY_VALUE(opponent_rating_range) AS opponent_rating_range,
+    ANY_VALUE(playing_as) AS playing_as,
+    ANY_VALUE(playing_result) AS playing_result,
+    ANY_VALUE(time_class) AS time_class,
     ANY_VALUE(median_score_playing_game_phase) AS median_score_playing_game_phase,
 FROM {{ ref ('games_with_moves') }}
-GROUP BY ALL
+GROUP BY 1, 2, 3
