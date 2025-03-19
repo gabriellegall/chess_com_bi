cf493ed8fedc4ca0926126e877581f7484f7155d
Gabriel LE GALL <gabrielgall@hotmail.fr>
2025-03-19 18:15:41 +0100

    added analysis for benchmark

--- analyses/audit_games_benchmark.sql
+++ analyses/audit_games_benchmark.sql
@@ -1,20 +1,50 @@
 -- SQL query to control the blunder rates calculations in Metabase against the source data
--- E.g. Rate Massive Blunder:
+-- game_phase can be added to control this aggregation level too.
 
-WITH agg_data AS (
+WITH agg_data_username AS (
   SELECT 
+    -- game_phase,
     COUNT(DISTINCT game_uuid) AS nb_games,
-    COUNT(DISTINCT CASE WHEN miss_category_playing = 'Massive Blunder' THEN game_uuid ELSE NULL END) AS nb_games_massive_blunder
-  FROM {{ ref("agg_games_with_moves") }}
+    COUNT(DISTINCT CASE WHEN miss_category_playing IN ('Blunder','Massive Blunder') THEN game_uuid ELSE NULL END) AS nb_games_blunder,
+    COUNT(DISTINCT CASE WHEN miss_category_playing = 'Massive Blunder' THEN game_uuid ELSE NULL END) AS nb_games_massive_blunder,
+  FROM {{ ref("games_with_moves") }}
   WHERE TRUE
     AND username = 'Zundorn'
     AND time_class = 'blitz'
     AND end_time_date >= DATE_TRUNC(CURRENT_DATE - INTERVAL 1 MONTH, MONTH)
     AND playing_rating_range = opponent_rating_range
     AND playing_rating_range = '600-700'
+  GROUP BY ALL
+)
+
+, agg_data_benchmark AS (
+  SELECT 
+    -- game_phase,
+    COUNT(DISTINCT game_uuid) AS nb_games,
+    COUNT(DISTINCT CASE WHEN miss_category_playing IN ('Blunder','Massive Blunder') THEN game_uuid ELSE NULL END) AS nb_games_blunder,
+    COUNT(DISTINCT CASE WHEN miss_category_playing = 'Massive Blunder' THEN game_uuid ELSE NULL END) AS nb_games_massive_blunder,
+  FROM {{ ref("games_with_moves") }}
+  WHERE TRUE
+    AND username <> 'Zundorn'
+    AND time_class = 'blitz'
+    -- AND end_time_date >= DATE_TRUNC(CURRENT_DATE - INTERVAL 1 MONTH, MONTH)
+    AND playing_rating_range = opponent_rating_range
+    AND playing_rating_range = '600-700'
+  GROUP BY ALL
 )
 
 SELECT
+  "username",
   *,
-  nb_games_massive_blunder / nb_games AS rate_massive_blunder
-FROM agg_data
+  nb_games_blunder / nb_games AS rate_massive_blunder,
+  nb_games_massive_blunder / nb_games AS rate_massive_blunder,
+FROM agg_data_username
+
+UNION ALL 
+
+SELECT
+  "benchmark",
+  *,
+  nb_games_blunder / nb_games AS rate_massive_blunder,
+  nb_games_massive_blunder / nb_games AS rate_massive_blunder,
+FROM agg_data_benchmark
